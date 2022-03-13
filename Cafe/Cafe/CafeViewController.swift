dde50132e1a543bdcb7dde7d88adfd4af4803c64
sanghee <anshee97@naver.com>
2022-03-13 15:22:10 +0900

    [Selina]

--- Cafe/Cafe/CafeViewController.swift
+++ Cafe/Cafe/CafeViewController.swift
@@ -8,99 +8,85 @@
 import UIKit
 
 class CafeViewController: UIViewController {
-
-    
-//    let menuLabel: UILabel = {
+//
+//    let guestView: UIView = {
+//        let view = UIView()
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.backgroundColor = .systemGray6
+//        return view
+//    }()
+//
+//    let guestName: UILabel = {
 //        let label = UILabel()
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.textColor = .black
 //        label.text = "메뉴"
-//        label.backgroundColor = .systemGray6
 //        return label
 //    }()
-    
-    
-    
-
-
-
-
-    let guestView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .systemGray6
-        return view
-    }()
-
-    let guestName: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .black
-        label.text = "메뉴"
-        return label
-    }()
-
-    let buyStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.backgroundColor = .clear
-        stackView.alignment = .center
-        stackView.spacing = 5
-        return stackView
-    }()
-
-    let americanoButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle(" 아메리카노 ", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .systemGray3
-        return button
-    }()
-
-    let cafeLatteButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle(" 카페라떼 ", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .systemGray3
-        return button
-    }()
-
-    let FrappuccinoButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle(" 프라프치노 ", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .systemGray3
-        return button
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        layout()
-    }
-
-    func layout() {
-        let safeAreaGuide = self.view.safeAreaLayoutGuide
-        self.view.addSubview(guestView)
-        self.guestView.addSubview(guestName)
-        self.guestView.addSubview(buyStackView)
-        
-        guestView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 10).isActive = true
-        guestView.leftAnchor.constraint(equalTo: safeAreaGuide.leftAnchor, constant: 10).isActive = true
-        guestView.rightAnchor.constraint(equalTo: buyStackView.rightAnchor, constant: 10).isActive = true
-        guestView.bottomAnchor.constraint(equalTo: buyStackView.bottomAnchor, constant: 10).isActive = true
-        
-        guestName.topAnchor.constraint(equalTo: guestView.topAnchor, constant: 10).isActive = true
-        guestName.leftAnchor.constraint(equalTo: guestView.leftAnchor, constant: 10).isActive = true
-        
-        buyStackView.topAnchor.constraint(equalTo: guestName.bottomAnchor, constant: 10).isActive = true
-        buyStackView.leftAnchor.constraint(equalTo: guestView.leftAnchor, constant: 10).isActive = true
-        buyStackView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        
-        buyStackView.addArrangedSubview(americanoButton)
-        buyStackView.addArrangedSubview(cafeLatteButton)
-        buyStackView.addArrangedSubview(FrappuccinoButton)
-    }
+//
+//    let buyStackView: UIStackView = {
+//        let stackView = UIStackView()
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.backgroundColor = .clear
+//        stackView.alignment = .center
+//        stackView.spacing = 5
+//        return stackView
+//    }()
+//
+//    let americanoButton: UIButton = {
+//        let button = UIButton()
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.setTitle(" 아메리카노 ", for: .normal)
+//        button.setTitleColor(.black, for: .normal)
+//        button.backgroundColor = .systemGray3
+//        return button
+//    }()
+//
+//    let cafeLatteButton: UIButton = {
+//        let button = UIButton()
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.setTitle(" 카페라떼 ", for: .normal)
+//        button.setTitleColor(.black, for: .normal)
+//        button.backgroundColor = .systemGray3
+//        return button
+//    }()
+//
+//    let FrappuccinoButton: UIButton = {
+//        let button = UIButton()
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.setTitle(" 프라프치노 ", for: .normal)
+//        button.setTitleColor(.black, for: .normal)
+//        button.backgroundColor = .systemGray3
+//        return button
+//    }()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        layout()
+//    }
+//
+//    func layout() {
+//        let safeAreaGuide = self.view.safeAreaLayoutGuide
+//        self.view.addSubview(guestView)
+//        self.guestView.addSubview(guestName)
+//        self.guestView.addSubview(buyStackView)
+//
+//        guestView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 10).isActive = true
+//        guestView.leftAnchor.constraint(equalTo: safeAreaGuide.leftAnchor, constant: 10).isActive = true
+//        guestView.rightAnchor.constraint(equalTo: buyStackView.rightAnchor, constant: 10).isActive = true
+//        guestView.bottomAnchor.constraint(equalTo: buyStackView.bottomAnchor, constant: 10).isActive = true
+//
+//        guestName.topAnchor.constraint(equalTo: guestView.topAnchor, constant: 10).isActive = true
+//        guestName.leftAnchor.constraint(equalTo: guestView.leftAnchor, constant: 10).isActive = true
+//
+//        buyStackView.topAnchor.constraint(equalTo: guestName.bottomAnchor, constant: 10).isActive = true
+//        buyStackView.leftAnchor.constraint(equalTo: guestView.leftAnchor, constant: 10).isActive = true
+//        buyStackView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+//
+//        buyStackView.addArrangedSubview(americanoButton)
+//        buyStackView.addArrangedSubview(cafeLatteButton)
+//        buyStackView.addArrangedSubview(FrappuccinoButton)
+//    }
 
 }
 
