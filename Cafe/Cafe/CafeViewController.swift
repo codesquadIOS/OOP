f738011d5f0d7bcbb7f9704a9fdb732decace075
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-13 15:31:03 +0900

    [Mase] 뷰 액션 연결 완료

--- Cafe/Cafe/CafeViewController.swift
+++ Cafe/Cafe/CafeViewController.swift
@@ -8,85 +8,45 @@
 import UIKit
 
 class CafeViewController: UIViewController {
-//
-//    let guestView: UIView = {
-//        let view = UIView()
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//        view.backgroundColor = .systemGray6
-//        return view
-//    }()
-//
-//    let guestName: UILabel = {
-//        let label = UILabel()
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        label.textColor = .black
-//        label.text = "메뉴"
-//        return label
-//    }()
-//
-//    let buyStackView: UIStackView = {
-//        let stackView = UIStackView()
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        stackView.backgroundColor = .clear
-//        stackView.alignment = .center
-//        stackView.spacing = 5
-//        return stackView
-//    }()
-//
-//    let americanoButton: UIButton = {
-//        let button = UIButton()
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.setTitle(" 아메리카노 ", for: .normal)
-//        button.setTitleColor(.black, for: .normal)
-//        button.backgroundColor = .systemGray3
-//        return button
-//    }()
-//
-//    let cafeLatteButton: UIButton = {
-//        let button = UIButton()
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.setTitle(" 카페라떼 ", for: .normal)
-//        button.setTitleColor(.black, for: .normal)
-//        button.backgroundColor = .systemGray3
-//        return button
-//    }()
-//
-//    let FrappuccinoButton: UIButton = {
-//        let button = UIButton()
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.setTitle(" 프라프치노 ", for: .normal)
-//        button.setTitleColor(.black, for: .normal)
-//        button.backgroundColor = .systemGray3
-//        return button
-//    }()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        layout()
-//    }
-//
-//    func layout() {
-//        let safeAreaGuide = self.view.safeAreaLayoutGuide
-//        self.view.addSubview(guestView)
-//        self.guestView.addSubview(guestName)
-//        self.guestView.addSubview(buyStackView)
-//
-//        guestView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 10).isActive = true
-//        guestView.leftAnchor.constraint(equalTo: safeAreaGuide.leftAnchor, constant: 10).isActive = true
-//        guestView.rightAnchor.constraint(equalTo: buyStackView.rightAnchor, constant: 10).isActive = true
-//        guestView.bottomAnchor.constraint(equalTo: buyStackView.bottomAnchor, constant: 10).isActive = true
-//
-//        guestName.topAnchor.constraint(equalTo: guestView.topAnchor, constant: 10).isActive = true
-//        guestName.leftAnchor.constraint(equalTo: guestView.leftAnchor, constant: 10).isActive = true
-//
-//        buyStackView.topAnchor.constraint(equalTo: guestName.bottomAnchor, constant: 10).isActive = true
-//        buyStackView.leftAnchor.constraint(equalTo: guestView.leftAnchor, constant: 10).isActive = true
-//        buyStackView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-//
-//        buyStackView.addArrangedSubview(americanoButton)
-//        buyStackView.addArrangedSubview(cafeLatteButton)
-//        buyStackView.addArrangedSubview(FrappuccinoButton)
-//    }
+
+    @IBOutlet weak var americanoCountLabel: UILabel!
+    
+    @IBOutlet weak var cafelatteCountLabel: UILabel!
+    
+    @IBOutlet weak var prapuccinoCountLabel: UILabel!
+    
+    @IBOutlet weak var amountSumLabel: UILabel!
+    
+    
+    @IBAction func americanoPlusButtonTouched(_ sender: Any) {
+    }
+
+    @IBAction func americanoMinusButtonTouched(_ sender: Any) {
+    }
+    
+    
+    @IBAction func cafelattePlusButtonTouched(_ sender: Any) {
+    }
+    
+    @IBAction func cafelateMinusButtonTouched(_ sender: Any) {
+    }
+    
+    
+    @IBAction func prapuccinoPlusButtonTouched(_ sender: Any) {
+    }
+    
+    @IBAction func prapuccinoMinusButtonTouched(_ sender: Any) {
+    }
+    
+    
+    @IBAction func placeOrerButtonTouched(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 
 }
 
